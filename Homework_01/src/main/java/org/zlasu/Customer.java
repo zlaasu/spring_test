24809476634d759c67745622886ae5b8782b31e8
Janek <zlaasu@gmail.com>
2019-10-23 20:47:27 +0200

    [day 3, home, task 1]

---
+++ Homework_01/src/main/java/org/zlasu/Customer.java
@@ -0,0 +1,21 @@
+package org.zlasu;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Customer {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}
