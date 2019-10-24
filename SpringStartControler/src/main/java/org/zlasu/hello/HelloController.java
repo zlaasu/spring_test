5a2b79987687b6396955fe6601f7bcb6455bb290
Janek <zlaasu@gmail.com>
2019-10-24 10:08:07 +0200

    [day 4, part 2, task 1]

--- SpringStartControler/src/main/java/org/zlasu/hello/HelloController.java
+++ SpringStartControler/src/main/java/org/zlasu/hello/HelloController.java
@@ -2,7 +2,10 @@ package org.zlasu.hello;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Random;
 
 @Controller
 public class HelloController {
@@ -11,5 +14,14 @@ public class HelloController {
     @ResponseBody
     public String helloWorldAction() {
         return "Hello World";
+    }
+
+    @GetMapping("/random/{max}/{min}")
+    @ResponseBody
+    public String random(@PathVariable int max) {
+        Random random = new Random();
+        int i = random.nextInt(max) + 1;
+
+        return "random: " + i;
     }
 }
