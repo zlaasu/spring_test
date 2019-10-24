9a4a1c9dcd7eebb16c7dc6bf866cb8d459f7c464
Janek <zlaasu@gmail.com>
2019-10-24 16:33:44 +0200

    [day 4, home, task 2]

--- Homework_02/src/main/java/org/zlasu/header/HeaderController.java
+++ Homework_02/src/main/java/org/zlasu/header/HeaderController.java
@@ -1,8 +1,14 @@
 package org.zlasu.header;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HeaderController {
 
+    @RequestMapping("/showUserAgent")
+    public String showUserAgent() {
+        return "hello";
+    }
 }
